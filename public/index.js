ac95fbd965b68be1d7f177accfc95238473bcad2
Andrew Seward <awswrd@gmail.com>
2020-04-12 13:08:34 -0700

    Add a bunch more content to the landing page

--- public/index.js
+++ public/index.js
@@ -1,18 +1,34 @@
 const blue = '#2B58A1';
 const red = '#C63519';
 
-let foo = 1;
-
+let bgToggleState = 0;
 window.setInterval(
   function() {
-    if (foo === 0) {
-      foo = 1;
-      document.body.style.backgroundColor = red;
-    } else {
-      foo = 0;
+    if (bgToggleState === 0) {
+      bgToggleState = 1;
       document.body.style.backgroundColor = blue;
+    } else {
+      bgToggleState = 0;
+      document.body.style.backgroundColor = red;
     }
   },
-  600
+  800
 );
 
+let titleToggleState = 0;
+window.setInterval(
+  function() {
+    if (titleToggleState === 0) {
+      // unlit
+      titleToggleState = 1;
+      document.body.getElementsByTagName('h1')[0].style.color = '#AAA';
+      document.body.getElementsByTagName('h1')[0].style.textShadow = '0 0 3px #111';
+    } else {
+      // lit
+      titleToggleState = 0;
+      document.body.getElementsByTagName('h1')[0].style.color = '#EEE';
+      document.body.getElementsByTagName('h1')[0].style.textShadow = '0 0 20px #CCC';
+    }
+  },
+  1100
+);
