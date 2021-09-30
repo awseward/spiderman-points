5cec568ab8c2fb3f0f3e935de60723720d119d13
Andrew Seward <awswrd@gmail.com>
2021-09-30 09:04:35 -0700

    Remove this
    Didn't end up using it.

--- scripts/serverd.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-ru_port=${PORT:-4567}
-ru_server="${SERVER:-puma}"
-
-bundle exec rackup --server "${ru_server}" --port "${ru_port}" --daemonize --pid "$1" config.ru
