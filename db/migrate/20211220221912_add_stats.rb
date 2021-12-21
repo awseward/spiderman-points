2361c4a94cbff334f8bc9c6d6feeb8e17804d864
Andrew Seward <awswrd@gmail.com>
2021-12-20 16:07:56 -0800

    Add some basic stats views
    Mostly just a "notebook" at this point tbh…

---
+++ db/migrate/20211220221912_add_stats.rb
@@ -0,0 +1,104 @@
+class AddStats < ActiveRecord::Migration[6.1]
+  def up
+    execute <<-SQL
+      CREATE SCHEMA IF NOT EXISTS stats;
+
+      CREATE VIEW stats.all_users AS (
+        SELECT DISTINCT team_id, from_id AS user_id FROM points
+        UNION
+        SELECT DISTINCT team_id, to_id   AS user_id FROM points
+      );
+
+      CREATE VIEW stats.points_given AS (
+        SELECT
+          count(*)
+        , team_id
+        , from_id AS user_id
+        FROM points
+        GROUP BY 2,3
+      );
+
+      CREATE VIEW stats.points_received AS (
+        SELECT
+        count(*)
+        , team_id
+        , to_id AS user_id
+        FROM points
+        GROUP BY 2,3
+      );
+
+      CREATE OR REPLACE VIEW stats.points_scores AS (
+        WITH _scores AS (
+          SELECT
+            COALESCE(pg.team_id, pr.team_id) AS team_id
+          , COALESCE(pg.user_id, pr.user_id) AS user_id
+          , COALESCE(pg.count, 0) AS points_given
+          , COALESCE(pr.count, 0) AS points_received
+          FROM stats.points_given pg
+          FULL OUTER JOIN stats.points_received pr ON pg.user_id = pr.user_id
+        )
+        SELECT
+          _scores.*
+        , TRUNC(CASE points_received
+                  WHEN 0 THEN NULL
+                  ELSE points_given::DECIMAL / points_received
+                END
+                , 2) AS points_given_ratio
+        , TRUNC(CASE points_given
+                  WHEN 0 THEN NULL
+                  ELSE points_received::DECIMAL / points_given
+                END
+                , 2) AS points_received_ratio
+        FROM _scores
+      );
+
+      CREATE OR REPLACE VIEW stats.emoji AS (
+        SELECT DISTINCT
+          UNNEST(REGEXP_MATCHES(reason, ':[^: ]+:', 'ig'))
+        FROM points
+        ORDER BY 1
+      );
+
+      CREATE OR REPLACE VIEW stats.emoji_occurrences AS (
+        SELECT
+          points.id
+        , UNNEST(REGEXP_MATCHES(reason, ':[^: ]+:', 'ig')) AS emoji
+        FROM points
+        WHERE created_at >= '2021-01-01'
+          AND created_at < '2022-01-01'
+      );
+
+      CREATE OR REPLACE VIEW stats.emoji_frequency AS (
+        SELECT
+          UNNEST(REGEXP_MATCHES(reason, ':[^: ]+:', 'ig')) AS emoji
+        , COUNT(DISTINCT from_id) AS from_users
+        , COUNT(DISTINCT id) AS points
+        , COUNT(DISTINCT to_id) AS to_users
+        , COUNT(*) AS total_usages
+        , ARRAY_AGG(DISTINCT created_at) AS timestamps
+        FROM points
+        WHERE created_at >= '2021-01-01'
+        GROUP BY 1
+        ORDER BY 2 DESC, 3 DESC, 5 DESC, 1
+      );
+
+      CREATE OR REPLACE VIEW stats.points_histogram AS (
+        SELECT
+          DATE_PART('YEAR', created_at) || '-' || LPAD(DATE_PART('MONTH', created_at)::TEXT, 2, '0') AS month
+        , COUNT(*) AS value
+        , REPEAT('x', COUNT(*)::INTEGER) AS bar
+        FROM points
+        WHERE created_at >= '2021-01-01'
+          AND created_At < '2022-01-01'
+        GROUP BY 1
+        ORDER BY 1
+      );
+    SQL
+  end
+
+  def down
+    execute <<-SQL
+      DROP SCHEMA stats CASCADE;
+    SQL
+  end
+end
